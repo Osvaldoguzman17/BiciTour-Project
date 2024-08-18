48b2eb3bed167cafa6ffc21004b6071ed65bf4d0
Jose Miguel <132602041+miguelito1214@users.noreply.github.com>
2024-08-18 16:54:13 -0600

    Delete inicio_sesion directory

--- inicio_sesion/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-from .models import registroUsuario
-
-class registroUsuarioForm(forms.ModelForm):
-    class Meta:
-        model = registroUsuario
-        fields = ['correo', 'nombre', 'contraseña']
